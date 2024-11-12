16fb3d6e03b60aced5f872a9db713e599d4baee3
Савелий <vildanov.s@bk.ru>
2024-11-12 23:34:42 +0500

    Add SpiralTest

---
+++ cs/TagsCloudVisualization/TagsCloudVisualizationTests/SpiralTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization.TagsCloudVisualizationTests;
+
+public class SpiralTest
+{
+    [Test]
+    public void getNextPointOnSpiral_ShouldReturnNextPointOnSpiral()
+    {
+        Spiral spiral = new Spiral(new Point(0, 0));
+        Point point = new Point();
+        for (int i = 0; i < 2; i++)
+        {
+            point = spiral.getNextPointOnSpiral();
+        }
+
+        point.Should().Be(new Point(0, 1));
+    }
+}
