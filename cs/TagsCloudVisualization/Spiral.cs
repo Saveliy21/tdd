42f7bf7307b4ffc29d1bc92d0e508190ff15a064
Савелий <vildanov.s@bk.ru>
2024-11-12 23:35:07 +0500

    Add class Spiral

---
+++ cs/TagsCloudVisualization/Spiral.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization;
+
+public class Spiral
+{
+    private readonly Point center;
+    private readonly int angleStep;
+    private double angle;
+
+
+    public Spiral(Point center, int angleStep = 1)
+    {
+        this.center = center;
+        this.angleStep = angleStep;
+        angle = 0;
+    }
+
+    public Point getNextPointOnSpiral()
+    {
+        angle += angleStep;
+        var x = (int) (center.X + angle * Math.Cos(angle));
+        var y = (int) (center.Y + angle * Math.Sin(angle));
+        return new Point(x, y);
+    }
+}
