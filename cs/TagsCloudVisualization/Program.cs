492c16ec3a10062b70974957119680b6fda1afad
Савелий <vildanov.s@bk.ru>
2024-11-16 16:19:16 +0500

    create Program.cs

---
+++ cs/TagsCloudVisualization/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+using TagsCloudVisualization;
+const string fileName1 = "Rectangles 50.png";
+const string fileName2 = "Rectangles 100.png";
+const string fileName3 = "Rectangles 400.png";
+
+CloudGenerator.DrawRectangles(CloudGenerator.GenerateRectangles(50), fileName1);
+CloudGenerator.DrawRectangles(CloudGenerator.GenerateRectangles(100), fileName2);
+CloudGenerator.DrawRectangles(CloudGenerator.GenerateRectangles(400), fileName3);
