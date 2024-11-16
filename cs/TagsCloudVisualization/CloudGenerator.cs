069d48f0210431479b5c0ad126d9796b457ebab5
Савелий <vildanov.s@bk.ru>
2024-11-16 16:18:39 +0500

    create CloudGenerator.cs

---
+++ cs/TagsCloudVisualization/CloudGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization;
+
+public static class CloudGenerator
+{
+    public static List<Rectangle> GenerateRectangles(int count)
+    {
+        CircularCloudLayouter circularCloudLayouter = new CircularCloudLayouter(new Point(500, 500));
+        Random rnd = new Random();
+        for (int i = 0; i < count; i++)
+        {
+            int width = rnd.Next(10, 25);
+            int height = rnd.Next(10, 25);
+            circularCloudLayouter.PutNextRectangle(new Size(width, height));
+        }
+
+        return circularCloudLayouter.GetRectangles();
+    }
+
+    public static void DrawRectangles(List<Rectangle> rectangles, string fileName)
+    {
+        Bitmap bmp = new Bitmap(1000, 1000);
+        Graphics newGraphics = Graphics.FromImage(bmp);
+        var backgroundBrush = new SolidBrush(Color.White);
+        var rectanglePen = new Pen(Color.Black);
+        newGraphics.FillRectangle(backgroundBrush, 0, 0, 1000, 1000);
+        foreach (var rectangle in rectangles)
+        {
+            newGraphics.DrawRectangle(rectanglePen, rectangle);
+        }
+
+        bmp.Save(fileName);
+    }
+}
