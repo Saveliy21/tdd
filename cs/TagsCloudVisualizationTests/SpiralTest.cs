27c162dabc56027ee3f67b271ae44eb23e0f58e3
Савелий <vildanov.s@bk.ru>
2024-11-16 23:48:05 +0500

    refactor SpiralTest.cs

--- cs/TagsCloudVisualizationTests/SpiralTest.cs
+++ cs/TagsCloudVisualizationTests/SpiralTest.cs
@@ -6,38 +6,21 @@ namespace TagsCloudVisualization.TagsCloudVisualizationTests;
 
 public class SpiralTest
 {
-    private Spiral _spiral;
-
-    [SetUp]
-    public void Setup()
-    {
-        _spiral = new Spiral(new Point(0, 0));
-    }
-
-    [Test]
-    public void getNextPointOnSpiral_ShouldReturnNextPointOnSpiral()
-    {
-        Point point = new Point();
-        for (int i = 0; i < 2; i++)
-        {
-            point = _spiral.GetNextPointOnSpiral();
-        }
-
-        point.Should().Be(new Point(0, 1));
-    }
-
     [Test]
     public void GetNextPointOnSpiral_ShouldReturnPointsOnSpiral()
     {
-        List<Point> expected = new List<Point>
+        Spiral spiral = new Spiral(new Point(CloudLayouterConst.CloudCentreX, CloudLayouterConst.CloudCentreY));
+        List<Point> expected =
+        [
+            new Point(500, 500), new Point(500, 500), new Point(499, 501), new Point(497, 500), new Point(497, 496),
+            new Point(501, 495), new Point(505, 498)
+        ];
+        List<Point> points = [];
+        for (int i = 0; i < 700; i++)
         {
-            new Point(0, 0), new Point(0, 1), new Point(-2, 0), new Point(-2, -3), new Point(1, -4),
-            new Point(5, -1), new Point(5, 4), new Point(-1, 7)
-        };
-        List<Point> points = new List<Point>();
-        for (int i = 0; i < 8; i++)
-        {
-            points.Add(_spiral.GetNextPointOnSpiral());
+            var point = spiral.GetNextPointOnSpiral();
+            if (i % 100 == 0)
+                points.Add(point);
         }
 
         points.Should().BeEquivalentTo(expected);
