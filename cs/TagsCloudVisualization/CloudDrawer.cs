0119e8dfedb4a5ef8d3f0baea40d8220f54bafdb
Савелий <vildanov.s@bk.ru>
2024-11-21 14:01:22 +0500

    replace CloudGenerator.cs to CloudDrawer.cs

---
+++ cs/TagsCloudVisualization/CloudDrawer.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization;
+
+public static class CloudDrawer
+{
+    public static Bitmap DrawRectangles(List<Rectangle> rectangles)
+    {
+        Bitmap bmp = new Bitmap(1000, 1000);
+        Graphics newGraphics = Graphics.FromImage(bmp);
+        var backgroundBrush = new SolidBrush(Color.White);
+        var rectanglePen = new Pen(Color.Black);
+        newGraphics.FillRectangle(backgroundBrush, 0, 0, 1000, 1000);
+        foreach (var rectangle in rectangles)
+        {
+            newGraphics.DrawRectangle(rectanglePen, rectangle);
+        }
+
+        newGraphics.Dispose();
+        return bmp;
+    }
+}
